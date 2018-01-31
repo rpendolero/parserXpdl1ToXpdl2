8d6d3aa2575f9d62efdda372e208c5db75110d72
rpendolero <pendolero@gmail.com>
2018-01-31 16:49:34 +0100

    Prueba de subida a Github

--- src/main/java/com/ust/parser/ParserApplications.java
+++ src/main/java/com/ust/parser/ParserApplications.java
@@ -5,7 +5,7 @@ import org.wfmc.xpdl.Applications;
 public class ParserApplications {
 
 	public static org.wfmc.xpdl2.Applications createApplications(Applications applications) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
